3c8ad49096c2d915557d3588611a7d8bc2945c48
minkyungbae <bmk0703@naver.com>
2025-01-27 19:09:32 +0900

    Final code

--- Homework/one_line_of_prompt.py
+++ Homework/one_line_of_prompt.py
@@ -6,18 +6,20 @@ tokenizer = AutoTokenizer.from_pretrained("meta-llama/Llama-2-7b-chat-hf")
 # CPU, GPU, Auto 해보기
 model = AutoModelForCausalLM.from_pretrained(
     "meta-llama/Llama-2-7b-chat-hf",
-    device_map = "cpu",
+    device_map = "auto",
+    offload_folder="offload",
+    offload_state_dict=True
     )
 
 # 프롬프트 명령
-prompt = "You are a very kind tour guide"
+prompt = "Answer as a very kind tour guide."
 
 # 입력 메시지
-input_text = "I'm going to travel to Korea, where should i go??"
+input_text = prompt +" I'm going to travel to Korea, where should i go??"
 inputs = tokenizer(input_text, return_tensors="pt")
 
 # 텍스트 생성
-outputs = model.generate(**inputs, max_length=50)   # 답변 길이는 50으로 제한
+outputs = model.generate(**inputs, max_length=100)   # 답변 길이는 100으로 제한
 response = tokenizer.decode(outputs[0], skip_special_tokens=True)
 
 # 출력
